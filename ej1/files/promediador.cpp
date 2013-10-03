19f96c02a5b089a93aee299e2fc460adb5511f65
fzanollo <fzanollo@dc.uba.ar>
2013-10-03 01:58:52 -0300

    ej1 finished

--- ej1/files/promediador.cpp
+++ ej1/files/promediador.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 
-#define casos 90
-
 using namespace std;
 
 int main()
 {	
 	int cant_datos, cant_paquetes;
 	unsigned long long int ciclos;
+	int cant_casos;
 	cin>> cant_datos;
+	cin>> cant_casos;
 	
-	unsigned long long int promedios[casos+1];
-	for(int i=0; i<casos; i++){	
+	unsigned long long int promedios[cant_casos+1];
+	for(int i=0; i<cant_casos; i++){	
 		promedios[i]= 0;
 	}
 	
-	for(int i=0; i<cant_datos*casos; i++){
+	for(int i=0; i<cant_datos*cant_casos; i++){
 		cant_paquetes=0;
 		ciclos=0;
 		cin>> cant_paquetes;
@@ -23,11 +23,11 @@ int main()
 		promedios[cant_paquetes-1] += ciclos; 	//sumo todos los valores de esa cant de paquetes
 	}
 	
-	for(int i=0; i<casos; i++){	//para cada caso
+	for(int i=0; i<cant_casos; i++){	//para cada caso
 		promedios[i]= promedios[i]/cant_datos; 	//divido por la cant de datos (saco el promedio)
 	}
 	
-	for(int i=0; i<casos; i++){	//para cada caso
+	for(int i=0; i<cant_casos; i++){	//para cada caso
 		cout<< i+1<< " " << promedios[i]<< endl;	//imprimo la cant de paquetes y el promedio
 	}
 	
